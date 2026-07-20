4c2008e27b9021dc6e42fb35a716ce846d16fbc9
Lok Tin Wong <loktinw0620@gmail.com>
2026-07-20 16:02:46 -0700

    Edited comment

--- src/Message.tsx
+++ src/Message.tsx
@@ -6,7 +6,7 @@ function Message() {
     const name = 'User';
     
     if (name) {
-        // This JSX, for JavaScript XML. Allows us to write HTML in React. HTML eventually gets converted into JavaScript.
+        // This is JSX, which stands for JavaScript XML. Allows us to write HTML in React. HTML eventually gets converted into JavaScript.
         return <h1>Hello {name}</h1>;
     } else {
         return <h1>Hello World</h1>;
