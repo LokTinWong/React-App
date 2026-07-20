0bcce8aae46665d9a29a09cafea5d4a5f8a61407
Lok Tin Wong <loktinw0620@gmail.com>
2026-07-20 16:28:24 -0700

    Switched to importing bootstrap.css

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+import 'bootstrap/dist/css/bootstrap.css'
 
 // ReactDOM renders the the App component (which is wrapped by the StrictMode component) in the element with the id 'root'.
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
