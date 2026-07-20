31da5d161e052776c8d9caec7b0bbb1d8b1fa654
Lok Tin Wong <loktinw0620@gmail.com>
2026-07-20 16:05:00 -0700

    Added comment

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+// ReactDOM renders the the App component (which is wrapped by the StrictMode component) in the element with the id 'root'.
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
